41912e12f012843ba88d0e146525cef6b1b5fdca
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-08-29 18:20:29 +0530

    Create CreateLinkedList.py
    This Python program implements a basic Singly Linked List data structure. It allows a user to dynamically specify how many items they want to store and then inserts those items one by one into the list.

---
+++ Linked Lists/CreateLinkedList.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None  
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+      
+
+a=int(input("Enter how many items you want to insert: "))
+lils=LinkedList()
+print("Enter items: ")
+for _ in range(a):
+    li=input("")
+    lils.append(li)
