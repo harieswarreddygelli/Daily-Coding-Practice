61e3013ed37ef04bf7b5dd6a0ceb24fc0e4063d7
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-08 18:15:37 +0530

    Create SumofMatrixexceptdiagonal.py
    This Python program prompts the user to define a matrix's dimensions and populate its elements, then calculates the sum of all elements except those on the main diagonal.

---
+++ Matrices/SumofMatrixexceptdiagonal.py
@@ -0,0 +1,17 @@
+r1 = int(input("Enter matrix1 rows size: "))
+c1 = int(input("Enter matrix1 columns size: "))
+
+matrix1 = []
+print("=" * 10, "Enter matrix 1 elements", "=" * 10)
+for i in range(r1):
+    rows = []
+    for j in range(c1):
+        rows.append(int(input(f"Element [{i}][{j}]: ")))
+    matrix1.append(rows)
+matrixsum=0
+for i in range(r1):
+    for j in range(c1):
+        if i!=j:
+            matrixsum+=matrix1[i][j]
+
+print("The total sum of  elements in the matrix except diagonals are: ",matrixsum)
