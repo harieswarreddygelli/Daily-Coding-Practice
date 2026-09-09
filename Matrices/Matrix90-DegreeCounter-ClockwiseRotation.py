70036a0829159c415e5d70f39b24acb65c3ecbdf
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-09 17:30:05 +0530

    Create Matrix90-DegreeCounter-ClockwiseRotation.py
    This Python script rotates a user-defined matrix by 90 degrees in the counter-clockwise (anti-clockwise) direction.

---
+++ Matrices/Matrix90-DegreeCounter-ClockwiseRotation.py
@@ -0,0 +1,25 @@
+r1 = int(input("Enter matrix1 rows size: "))
+c1 = int(input("Enter matrix1 columns size: "))
+
+matrix1 = []
+print("=" * 10, "Enter matrix 1 elements", "=" * 10)
+for i in range(r1):
+    rows = []
+    for j in range(c1):
+        rows.append(int(input(f"Element [{i}][{j}]: ")))
+    matrix1.append(rows)
+for row in matrix1:
+    row.reverse()
+    
+transpose=[]
+for i in range(c1):
+    tranele=[]
+    for j in range(r1):
+        tranele.append(matrix1[j][i])
+    transpose.append(tranele)
+
+
+    
+print("The Matrix 90-Degree Rotation in counter clock wise:")
+for row in transpose:
+    print(row)
