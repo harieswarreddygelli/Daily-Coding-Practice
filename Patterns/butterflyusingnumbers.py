f44a53c28d9344a894df31b8acf6198db83a06de
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-12 17:42:30 +0530

    Create butterflyusingnumbers.py
    This Python program generates a symmetric butterfly pattern using numbers.

---
+++ Patterns/butterflyusingnumbers.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a Number: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print(" " * (4 * (n - i)), end="")  
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+for i in range(n - 1, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print(" " * (4 * (n - i)), end="")  
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
