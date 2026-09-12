a0d7be0137902f35e039701d78555bbe5c3f3ee7
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-12 17:31:20 +0530

    Create Butterfly.py
    This Python program  prints a symmetric butterfly star pattern.

---
+++ Patterns/Butterfly.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a Number: "))
+for i in range(1, n + 1):
+    print("*" * i, end="")
+    print(" " * (2 * (n - i)), end="")
+    print("*" * i)
+for i in range(n, 0, -1):
+    print("*" * i, end="")
+    print(" " * (2 * (n - i)), end="")
+    print("*" * i)
