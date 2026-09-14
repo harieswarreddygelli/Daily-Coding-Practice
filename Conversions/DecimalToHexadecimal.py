be4928f5b5c8b1d3343cc4ef1e965d0f707c0c9b
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-14 13:46:02 +0530

    Create DecimalToHexadecimal.py
    This Python program prompts the user to enter an integer and demonstrates three built-in methods for displaying its hexadecimal (base-16) equivalent in both lowercase and uppercase formats.

---
+++ Conversions/DecimalToHexadecimal.py
@@ -0,0 +1,7 @@
+n = int(input('Enter  number: '))
+
+print("Hexadecimal of the given  number:",hex(n))
+
+print("Hexadecimal of the given number:",format(n,'x'))
+
+print("Hexadecimal of the given number:",format(n,'X'))
