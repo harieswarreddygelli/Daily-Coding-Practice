dfea8d0c6193901423fc92df34022b8171dbea6c
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-15 21:42:50 +0530

    Create OctalToHexaDecimal.py
    This Python program takes an octal number (base-8) entered by the user and converts it into its equivalent hexadecimal (base-16) representation using three different built-in methods.

---
+++ Conversions/OctalToHexaDecimal.py
@@ -0,0 +1,7 @@
+n=input("Enter Octal Number: ")
+
+print("HexaDecimal  of the Give Octal Number:",hex(int(n,8)))
+
+print("HexaDecimal of the Give Octal Number:",format((int(n,8)),'x'))
+
+print("HexaDecimal of the Give Octal Number:",format((int(n,8)),'X'))
