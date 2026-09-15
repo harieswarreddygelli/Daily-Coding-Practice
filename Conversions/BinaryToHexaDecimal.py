5e5a72cbdd7fd8202514d3ccc824a69a3c4af2c1
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-15 21:36:23 +0530

    Create BinaryToHexaDecimal.py
    This Python program takes a binary number (base-2) entered by the user and converts it into its equivalent hexadecimal (base-16) representation using three different built-in methods.

---
+++ Conversions/BinaryToHexaDecimal.py
@@ -0,0 +1,7 @@
+n=input("Enter Binary Number: ")
+
+print("HexaDecimal  of the Give Binary Number:",hex(int(n,2)))
+
+print("HexaDecimal of the Give Binary Number:",format((int(n,2)),'x'))
+
+print("HexaDecimal of the Give Binary Number:",format((int(n,2)),'X'))
