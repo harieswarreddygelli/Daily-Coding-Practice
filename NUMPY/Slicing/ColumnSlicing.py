29cea0bff8bda08f392d69782573066efdd80a1b
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-26 23:03:40 +0530

    Create ColumnSlicing.py
    This Python code demontrate the Column slicing in a 2d matrix

---
+++ NUMPY/Slicing/ColumnSlicing.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a[:])
+# Used to print only rows then use a[: ,Colum number]
+# prints first Column
+print(a[:,0]) 
+# prints second Column
+print(a[:,1])
+# prints Third Column
+print(a[:,2]) 
