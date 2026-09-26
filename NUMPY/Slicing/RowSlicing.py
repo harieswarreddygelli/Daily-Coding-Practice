605aca6b29003471568497861a5c0d3225c15571
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-26 23:00:30 +0530

    Create RowSlicing.py
    This Python Code perfomes row slicing in 2d matrix

---
+++ NUMPY/Slicing/RowSlicing.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a[:])
+# Used to print only rows then use a[row number,:]
+# prints first row only
+print(a[0,:]) 
+# prints second row
+print(a[1,:])
+# prints Third row
+print(a[2,:])
