1d0d67bb5228ebe42f540fd4aeb7b38a10090564
Harieswar Reddy Gelli <harieswarreddygelli@gmail.com>
2026-09-26 23:13:42 +0530

    Create SpecificColumnSlicing.py
    This Python code Demonstrates how the Specific Columns Slicing Works.

---
+++ NUMPY/Slicing/SpecificColumnSlicing.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print The 2d Matrix First
+print(a[:])
+# Prints The Specified columns 
+# They are denoted as a[:,[Requried columns]]
+print(a[:,[0,2]])
+print(a[:,[0,1]])
+print(a[:,[1,2]])
